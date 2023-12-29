184c27c1faf8e97f3d4c9a5e70b34dc67e4a5ad4
ManashSwain <manash0802@gmail.com>
2023-12-29 23:04:10 +0530

    made changes in the tags

--- script.js
+++ script.js
@@ -16,10 +16,10 @@ function themechanger() {
     document.body.classList.toggle("dark-theme");
    if(document.body.classList.contains("dark-theme")){
            
-           mode.src="/images/crescent-moon.png";
+           mode.src="./images/crescent-moon.png";
    }
    else{
-        mode.src="/images/sunny.png";
+        mode.src="./images/sunny.png";
    }
    if((localStorage.getItem("theme") == "light")){
     localStorage.setItem("theme","dark");
@@ -31,11 +31,11 @@ function themechanger() {
 
 if(localStorage.getItem("theme") == "light"){
      document.body.classList.remove("dark-theme");
-     mode.src="/images/sunny.png";
+     mode.src="./images/sunny.png";
 }
 else if (localStorage.getItem("theme") == "dark"){
     document.body.classList.add("dark-theme");
-    mode.src="/images/crescent-moon.png";
+    mode.src="./images/crescent-moon.png";
 }
 else{
     localStorage.setItem("theme","light");
